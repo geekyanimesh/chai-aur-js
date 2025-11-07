fbdb9758b78926ade72c7a4cf7fc844e6b76c3d7
geekyanimesh <animesh.kumar9651@gmail.com>
2025-11-07 22:31:23 +0530

    variables

---
+++ 01_variables/index.js
@@ -0,0 +1,15 @@
+
+const account_Id = 1343243
+let accountEmail="animesh@gmail.com"
+var accountPass="34323"     // not used much
+accountCity="Ranchi"
+
+// account_Id = 2       // const can not be changed
+
+console.log(account_Id)
+
+console.table([account_Id, accountEmail, accountPass])
+
+/* 
+    Never use var : because of issues in scope 
+*/
