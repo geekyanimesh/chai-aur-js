fb8ecebf5c3d8f9b978acc7dc1792e6902dba448
geekyanimesh <animesh.kumar9651@gmail.com>
2025-11-07 23:02:50 +0530

    datatypes:

---
+++ 02_datatypes/index.js
@@ -0,0 +1,17 @@
+"use strict"    // treat all JS code as newer version
+
+// alert("hello")  // not working since not binded with html (used in browser)
+
+// data types
+
+/* 
+    number
+    bigINT
+    string
+    boolean
+    null
+    undefined
+    symbol
+*/
+
+console.log(typeof null)    // object
