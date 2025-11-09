3917c2d3bdc22643787badde590e19d5f0a0f7fc
geekyanimesh <animesh.kumar9651@gmail.com>
2025-11-09 12:23:00 +0530

    strings

---
+++ 08_strings/main.js
@@ -0,0 +1,15 @@
+
+const name = "Animesh";
+const myRepoCount=19;
+
+// Old way - Concatenation
+console.log("Hello my name is "+name+" and repo count "+myRepoCount);
+
+// New way - String intercoalition
+console.log(`Hello my name is ${name} and repo count ${myRepoCount}`);
+
+// ` ` - backticks and ${variable}
+
+const url  = "https://animesh/animesh%20kumar";
+console.log(url.replace('%20','-'));
+
