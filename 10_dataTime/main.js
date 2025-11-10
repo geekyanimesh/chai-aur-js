3d85a77764ddbece6cc28a597ea48e6cc64296fc
geekyanimesh <animesh.kumar9651@gmail.com>
2025-11-10 11:48:36 +0530

    date and time

---
+++ 10_dataTime/main.js
@@ -0,0 +1,13 @@
+
+let myDate = new Date();
+console.log(myDate.toString())
+
+console.log(myDate.toLocaleDateString())
+console.log(typeof myDate)
+
+let myCreatedDate = new Date(2025,10,9);
+console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+
