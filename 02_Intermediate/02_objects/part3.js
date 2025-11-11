9da63ed33cc70efd24c0fdc84dced726e9eac10c
geekyanimesh <animesh.kumar9651@gmail.com>
2025-11-11 18:02:10 +0530

    destructure of objects

---
+++ 02_Intermediate/02_objects/part3.js
@@ -0,0 +1,11 @@
+
+const course = {
+    coursename: "js",
+    coursefees: "999",
+    courseInstructor: 'hitesh'
+}
+
+const {courseInstructor: instructor} = course;
+// console.log(courseInstructor)
+console.log(instructor)
+
