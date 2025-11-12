d11dab534ce748bd6efa1c24404ffc54fe8c8b32
geekyanimesh <animesh.kumar9651@gmail.com>
2025-11-12 12:42:10 +0530

    scope part 1

---
+++ 02_Intermediate/04_scope/main.js
@@ -0,0 +1,13 @@
+
+// problem with var
+
+if(true){
+    let a = 10;
+    const b = 20;
+    var c = 30;
+}
+
+//console.log(a);   error
+//console.log(b);   error
+console.log(c);     // no error
+
