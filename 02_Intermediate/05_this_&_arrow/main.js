8e93124b03d9bfa61ef004162779d6be4c98fc03
geekyanimesh <animesh.kumar9651@gmail.com>
2025-11-13 22:39:18 +0530

    this

---
+++ 02_Intermediate/05_this_&_arrow/main.js
@@ -0,0 +1,11 @@
+
+const user = {
+    username: "Animesh",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} Welcome to the site`)
+    }
+}
+
+user.welcomeMessage();
