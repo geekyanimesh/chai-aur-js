8ffa075ef428b4917c467b3b6d158adbaa70d98c
geekyanimesh <animesh.kumar9651@gmail.com>
2025-11-15 13:55:59 +0530

    control flow

---
+++ 02_Intermediate/08_ControlFlow/truthy.js
@@ -0,0 +1,22 @@
+
+const userEmail  = 'animesh@gmail.com';
+
+if (userEmail) {
+    console.log("Got user mail");
+}else{
+    console.log("Not got user mail");
+}
+
+/* falsy values: 
+
+    false, 0, -0, BigInt, "", null, undefined, NaN
+
+*/
+
+/* truthy  values
+    
+    "0", 'false', " ", [], {}, function(){}
+
+ */
+
+    
